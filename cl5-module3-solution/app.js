ff376269bb581db3e3c14a5be8a3ea5300ce4834
yilin2548 <yilinliu@seas.upenn.edu>
2017-04-04 22:40:03 -0400

    WIP

---
+++ cl5-module3-solution/app.js
@@ -0,0 +1,75 @@
+(function (){
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective);
+
+function FoundItemsDirective() {
+	var ddo = {
+		templateUrl: 'menuItem.html',
+		scope: {
+			items: '<',
+			onRemove: '&'
+		}
+	};
+	return ddo;
+}
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService){
+	var list = this;
+	list.searchTerm ='';
+	
+	list.search = function () {
+		if (list.searchTerm == ''){
+			list.found = [];
+		}
+		else{
+			var promise = MenuSearchService.getMatchedMenuItems(list.searchTerm);
+
+			promise.then(function (response){
+				list.found = response;
+			})
+			.catch(function(error){
+				console.log("Something wrong.");
+			});	
+		}
+
+	};
+
+	list.remove = function (index) {
+		list.found.splice(index, 1);
+	};
+}
+
+MenuSearchService.$inject = ['$http'];
+function MenuSearchService($http){
+	var service = this;
+
+ 	service.getMatchedMenuItems = function (searchTerm) {
+	    var response = $http({
+	      method: "GET",
+	      url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+		});
+
+
+	    return response.then(function (response){
+	    	// process result and only keep item
+	        var foundItems = [];
+
+	    	var result = response.data.menu_items;
+	    	for (var i = 0; i < response.data.menu_items.length; i++) {
+		    	if (result[i].description.toLowerCase().indexOf(searchTerm) !== -1){
+		    		foundItems.push(result[i])
+		    	}
+		    }
+	    	return foundItems;
+	    });
+
+	};
+}
+
+
+})();
